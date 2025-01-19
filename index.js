178b60d796820a66cb53f2f6c6fc26d52b370aa6
NAJIYA KP <najiyabinthmajeed@gmail.com>
2025-01-19 03:19:55 -0800

    code updated

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+// config is a method to when the application run then load from env variable to process
+
+const express = require('express')
+const cors= require('cors')
+// client request to accepts in server side
+
+const router=require('./Routes/router')
+
+const rentServer = express()
+rentServer.use(cors())
+rentServer.use(express.json()) //convert json to js object (it act as middleware)
+rentServer.use(router)
+
+const PORT =3000 || process.env.PORT
+
+// to run
+rentServer.listen(PORT, ()=>{
+    console.log(`server running port ${PORT}`)
+    
+})
+
+rentServer.get('/',(req,res)=>{
+    res.status(200).send(`<h1>server started at port 3000</h1>`)
+   
+})
+// logic 
